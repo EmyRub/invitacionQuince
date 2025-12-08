32700ef84ed0220d5573d1a5335729613b611fae
EmyRub <esme.rubi@hotmail.com>
2025-12-08 17:27:55 -0600

    margenes

--- src/App.tsx
+++ src/App.tsx
@@ -186,15 +186,17 @@ function App() {
       </section>
 
       <section id="vestimenta" className="py-10 lg:py-18 bg-lila-dark text-white">
-        <h2 className="fade-in">Código de vestimenta</h2>
-        <div className="flex justify-center gap-4 my-8 fade-in">
-          <GiDress className="text-lila text-6xl fade-in" />
-          <FaHatCowboy className="text-lila text-6xl fade-in" />
-        </div>
-        <p className="text-lila fade-in">FORMAL</p>
+        <div className="mx-auto max-w-11/12 lg:max-w-5xl">
+          <h2 className="fade-in">Código de vestimenta</h2>
+          <div className="flex justify-center gap-4 my-8 fade-in">
+            <GiDress className="text-lila text-6xl fade-in" />
+            <FaHatCowboy className="text-lila text-6xl fade-in" />
+          </div>
+          <p className="text-lila fade-in">FORMAL</p>
 
-        <h3 className="mt-20 fade-in">Sugerencia</h3>
-        <p className="text-4xl! max-w-4xl mx-auto text-lila fade-in">Amablemente, el color lila únicamente para la quinceañera</p>
+          <h3 className="mt-20 fade-in">Sugerencia</h3>
+          <p className="text-2xl lg:text-4xl! max-w-4xl mx-auto text-lila fade-in">Amablemente, el color lila únicamente para la quinceañera</p>
+        </div>
       </section>
 
       <section className="template-section">
